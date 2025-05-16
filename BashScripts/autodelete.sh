cb273bae57e6b31c8e9f4ef5b7994a74fb915917
BlazeSam <lafiaji@Samuel>
2025-05-15 22:38:28 -0600

    Added and removed some old features, FOund the regex to find duplicate files and now I just neeed to code how to process them safely

--- BashScripts/autodelete.sh
+++ BashScripts/autodelete.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-find . -empty | xargs rm
-echo Love
+#find . -empty | xargs rm
+
+# if [[ "$#" -ne 1 ]]; then
+	# echo "the deleter needs to be given the directory"
+	# exit 1
+# fi
+
+echo Love $1
+eif 1[[ test -f recent.tar ]]
+find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+'
+
