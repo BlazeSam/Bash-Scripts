8401101396486ff13992e40aaccd1a4cce5478c5
BlazeSam <154655819+BlazeSam@users.noreply.github.com>
2025-05-27 00:39:04 -0600

    Update autodelete.sh
    Add new comments to explain what it does

--- BashScripts/autodelete.sh
+++ BashScripts/autodelete.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-#find . -empty | xargs rm
-
-# if [[ "$#" -ne 1 ]]; then
-	# echo "the deleter needs to be given the directory"
-	# exit 1
-# fi
 
 echo Love $1
 
 if [[ ! -e del.tar.gz ]]; then
-	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -czvf del.tar.gz 
+	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -czvf del.tar.gz  #regex for finding duplicate files and backing them up
 elif [[  -e del.tar.gz ]]; then
-	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -rzvf del.tar.gz 
+	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -rzvf del.tar.gz # regex for incase  the tar file all ready exists
 fi
