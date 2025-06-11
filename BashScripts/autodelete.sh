26107bb3a0d79cd3440a3627c3a6f946419a8aad
BlazeSam <lafiaji@Samuel.localdomain>
2025-06-11 00:15:21 -0600

    reworking the script

--- BashScripts/autodelete.sh
+++ BashScripts/autodelete.sh
@@ -7,9 +7,21 @@
 # fi
 
 echo Love $1
+if [ -n "$1"]; then
+	echo moving to $1 directory
+else
+	echo "Ensure directory is provided"
+fi 
+
+cd "$(find . -type d -name $1 -print -quit)"
+
+mkdir -p backup
 
 if [[ ! -e del.tar.gz ]]; then
-	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -czvf del.tar.gz 
+	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' -print0 |
+	while IFS= read -r -d ' ' file; do
+		mv "$file" ./backup
+	done	
 elif [[  -e del.tar.gz ]]; then
 	find . -regextype posix-extended -type f -regex '.* \(+[0-9]+\)\.[^/]+' | xargs tar -rzvf del.tar.gz 
 fi
